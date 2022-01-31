12549fe866ff1259184179e641b618224da72d53
Javier-Acosta <javitoacosta@gmail.com>
2022-01-31 11:26:35 -0300

    Logo Moneda Virtuales

--- valan/src/app/app.routes.ts
+++ valan/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { SesionComponent } from './components/sesion/sesion.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { SomosComponent } from './components/somos/somos.component';
 import { AyudaComponent } from './components/ayuda/ayuda.component';
+import { MenuPrincipalComponent } from './components/menuPrincipal/menuPrincipal.component';
+
 
 
 export const ROUTES: Routes= [
@@ -13,6 +15,7 @@ export const ROUTES: Routes= [
     {path:'sesion', component: SesionComponent},
     {path:'registro',component: RegistroComponent },
     {path:'somos',component: SomosComponent },
+    {path:'menu',component: MenuPrincipalComponent  },
     {path:'ayuda',component: AyudaComponent },
     {path:'', pathMatch: 'full', redirectTo:'home'},
     {path:'**', pathMatch: 'full', redirectTo:'home'},    
